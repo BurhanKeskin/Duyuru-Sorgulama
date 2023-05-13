268b9c7869c136071271c829cb7da7a90ec09f1c
Burhan Keskin <101925448+BurhanKeskin@users.noreply.github.com>
2023-05-13 08:14:35 +0300

    #

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 from bs4 import BeautifulSoup
+import requests
+
+from datetime import date
+import locale
+
+# Yerel zaman dilimini Türkiye için ayarladım
+# I've set the local time zone for Turkey
+locale.setlocale(locale.LC_TIME, "tr_TR")
+
+# Bugünün tarihini değişkene atadım.
+# I've assigned the today's date into the variable 
+currentDate = date.today().strftime("%d %B %Y")
+# ayrıca bugünü kelime olarakta değişkene atadım.
+# also I've assigned the today into the variable as string
+currentDay = date.today().strftime("%A")
+
+
+
+
+print(f"{currentDate} {currentDay}")
+
+AnnouncementLinks = []
+AnnouncementTitles = []
+
+
